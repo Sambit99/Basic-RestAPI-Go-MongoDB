5a90245590b6c45c0c66b423e799c14d39027134
Sambit99 <sambitkumar75@gmail.com>
2025-08-19 22:24:48 +0530

    fix: Add missing BSON tags to User struct fields

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/v2/bson"
 
 type User struct {
 	ID     bson.ObjectID `json:"id,omitempty" bson:"_id"`
-	Name   string        `json:"name"`
-	Age    int           `json:"age"`
-	Gender string        `json:"gender"`
+	Name   string        `json:"name" bson:"name"`
+	Age    int           `json:"age" bson:"age"`
+	Gender string        `json:"gender" bson:"gender"`
 }
