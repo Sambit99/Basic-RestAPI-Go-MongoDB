0854c5760f7391975dcdf32395c460b72a84ee71
Sambit99 <sambitkumar75@gmail.com>
2025-08-19 22:36:23 +0530

    feat: Add user controller with CRUD function stubs

---
+++ pkg/controllers/user-controller.go
@@ -0,0 +1,9 @@
+package controllers
+
+import "net/http"
+
+func GetAllUser(w http.ResponseWriter, r *http.Request)  {}
+func GetUserById(w http.ResponseWriter, r *http.Request) {}
+func CreateUser(w http.ResponseWriter, r *http.Request)  {}
+func DeleteUser(w http.ResponseWriter, r *http.Request)  {}
+func UpdateUser(w http.ResponseWriter, r *http.Request)  {}
