b5ac02174c17a6cd47019e6fe26fb64a4260fc90
Sambit99 <sambitkumar75@gmail.com>
2025-08-19 22:36:13 +0530

    feat: Implement user routes for CRUD operations

---
+++ pkg/routes/user-routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/Sambit99/Basic-RestAPI-Go-MongoDB/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterUserRoutes = func(r *mux.Router) {
+	r.HandleFunc("/user", controllers.GetAllUser).Methods("GET")
+	r.HandleFunc("/user/{userId}", controllers.GetUserById).Methods("GET")
+	r.HandleFunc("/user", controllers.CreateUser).Methods("POST")
+	r.HandleFunc("/user", controllers.UpdateUser).Methods("PUT")
+	r.HandleFunc("/user", controllers.DeleteUser).Methods("DELETE")
+}
