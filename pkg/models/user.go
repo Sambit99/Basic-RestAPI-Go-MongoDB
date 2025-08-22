ee47a0947bc16ce540806480ff5608ba9eb062e9
Sambit99 <sambitkumar75@gmail.com>
2025-08-22 17:57:13 +0530

    feat: Refactor user model and implement GetAllUser function for retrieving users from the database

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,10 +1,48 @@
 package models
 
-import "go.mongodb.org/mongo-driver/v2/bson"
+import (
+	"context"
+	"log"
+
+	"github.com/Sambit99/Basic-RestAPI-Go-MongoDB/pkg/config"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var db *mongo.Collection
+var ctx = context.Background()
 
 type User struct {
-	ID     bson.ObjectID `json:"id,omitempty" bson:"_id"`
+	ID     bson.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name   string        `json:"name" bson:"name"`
 	Age    int           `json:"age" bson:"age"`
 	Gender string        `json:"gender" bson:"gender"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+}
+
+func GetAllUser() []bson.M {
+	cursor, err := db.Find(ctx, bson.M{}, nil)
+
+	if err != nil {
+		log.Fatal("Error while getting all users", err.Error())
+	}
+	defer cursor.Close(ctx)
+
+	var users []bson.M
+
+	for cursor.Next(ctx) {
+		var user bson.M
+
+		if err = cursor.Decode(&user); err != nil {
+			log.Fatal("Error while decoding user", err.Error())
+		}
+
+		users = append(users, user)
+	}
+
+	return users
 }
