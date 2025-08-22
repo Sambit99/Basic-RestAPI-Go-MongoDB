ff3cd8f371c269e15adcee7803de1d01760a579a
Sambit99 <sambitkumar75@gmail.com>
2025-08-22 17:56:30 +0530

    feat: Implement GetAllUser function to retrieve and return all users in JSON format

--- pkg/controllers/user-controller.go
+++ pkg/controllers/user-controller.go
@@ -1,9 +1,16 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
-func GetAllUser(w http.ResponseWriter, r *http.Request)  {}
-func GetUserById(w http.ResponseWriter, r *http.Request) {}
-func CreateUser(w http.ResponseWriter, r *http.Request)  {}
-func DeleteUser(w http.ResponseWriter, r *http.Request)  {}
-func UpdateUser(w http.ResponseWriter, r *http.Request)  {}
+	"github.com/Sambit99/Basic-RestAPI-Go-MongoDB/pkg/models"
+)
+
+func GetAllUser(w http.ResponseWriter, r *http.Request) {
+	users := models.GetAllUser()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(users)
+}
