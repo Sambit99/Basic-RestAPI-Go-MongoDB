fdec6fa274bdd08d0caf69ac316db7cd924f0683
Sambit99 <sambitkumar75@gmail.com>
2025-08-22 17:57:38 +0530

    feat: Implement GetUserById function to retrieve a user by ID from the database

--- pkg/controllers/user-controller.go
+++ pkg/controllers/user-controller.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/Sambit99/Basic-RestAPI-Go-MongoDB/pkg/models"
+	"github.com/gorilla/mux"
 )
 
 func GetAllUser(w http.ResponseWriter, r *http.Request) {
@@ -13,4 +14,15 @@ func GetAllUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(users)
+}
+func GetUserById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	userId := params["userId"]
+
+	user := models.GetUserById(userId)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
 }
