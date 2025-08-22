264df17b419de4b6dffc0fce82cc6423bd2df629
Sambit99 <sambitkumar75@gmail.com>
2025-08-22 21:17:25 +0530

    fix: Correct UpdateUser route to include userId in the URL path

--- pkg/routes/user-routes.go
+++ pkg/routes/user-routes.go
@@ -9,6 +9,6 @@ var RegisterUserRoutes = func(r *mux.Router) {
 	r.HandleFunc("/user", controllers.GetAllUser).Methods("GET")
 	r.HandleFunc("/user/{userId}", controllers.GetUserById).Methods("GET")
 	r.HandleFunc("/user", controllers.CreateUser).Methods("POST")
-	r.HandleFunc("/user", controllers.UpdateUser).Methods("PUT")
+	r.HandleFunc("/user/{userId}", controllers.UpdateUser).Methods("PUT")
 	r.HandleFunc("/user/{userId}", controllers.DeleteUser).Methods("DELETE")
 }
